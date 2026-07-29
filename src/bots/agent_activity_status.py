7dedd7b9f2c95c9581926c62597579c289aa1d89
Alinawaz Husain <alinawazhusain07@gmail.com>
2026-07-29 16:09:07 +0530

    debug

--- src/bots/agent_activity_status.py
+++ src/bots/agent_activity_status.py
@@ -9,11 +9,13 @@ from models.agent_model import Agents
 
 async def check_active(agent_name):
     result = await mqtt_request(agent_name=agent_name, command =  "active_test",timeout=10.0)
+    print(result)
     return True
 
 
 
 async def check_active_status():
+    print("stareted")
     while(True):
         async with get_async_session() as session:
                     res = await session.execute(select(Agents))
