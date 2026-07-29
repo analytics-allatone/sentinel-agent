a949bf79486f1b98b769e2e10dbcf82cd8c0f962
Alinawaz Husain <alinawazhusain07@gmail.com>
2026-07-29 16:15:40 +0530

    debug1

--- src/bots/agent_activity_status.py
+++ src/bots/agent_activity_status.py
@@ -9,8 +9,9 @@ from models.agent_model import Agents
 
 async def check_active(agent_name):
     result = await mqtt_request(agent_name=agent_name, command =  "active_test",timeout=10.0)
-    print(result)
-    return True
+    res = result != None
+    print(res)
+    return res
 
 
 
